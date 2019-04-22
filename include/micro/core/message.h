8455a53217f971bfd926255ce2ea5f2b751f9548
Felipe Ortiz <fortizc@gmail.com>
2019-04-22 13:45:19 -0400

    Add basic class to send messages (request or responses) in Micro

--- include/micro/core/message.h
+++ include/micro/core/message.h
@@ -9,13 +9,33 @@ namespace micro
     {
     public:
         Message(unsigned iface_id,
-                unsigned iface_type,
+                const std::string &iface_support,
                 const std::string &metadata,
                 const std::string &data);
 
+        unsigned get_iface_id() const
+        {
+            return iface_id;
+        }
+
+        const std::string get_iface_support() const
+        {
+            return iface_support;
+        }
+
+        const std::string get_metadata() const
+        {
+            return metadata;
+        }
+
+        const std::string get_data() const
+        {
+            return data;
+        }
+
     protected:
         unsigned iface_id;
-        unsigned iface_type;
+        std::string iface_support;
         std::string metadata;
         std::string data;
     };
