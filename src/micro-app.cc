9cc22a79178fee0dfbc433523cec98bbfca9819a
Felipe Ortiz <fortizc@gmail.com>
2019-04-24 02:49:14 -0400

    Create a C readable interface
    This implementation point to get a better integration with C and other
    languages only changing the base class but not the loader implementation

--- src/micro-app.cc
+++ src/micro-app.cc
@@ -1,53 +1,15 @@
 #include <micro/app/micro-app.h>
 
 
-micro::MicroApp::MicroApp(const std::string &name, const std::vector<std::string> &iface_types)
+micro::MicroApp::MicroApp(char *name)
     : name(name)
-    , iface_types(iface_types)
+    , iface_types(nullptr)
+    , version(nullptr)
+    , url(nullptr)
+    , author(nullptr)
+    , email(nullptr)
+    , description(nullptr)
+    , long_description(nullptr)
+    , help(nullptr)
 {
-}
-
-const std::string micro::MicroApp::get_name() const
-{
-    return name;
-}
-
-const std::vector<std::string> micro::MicroApp::get_types() const
-{
-    return iface_types;
-}
-
-const std::string micro::MicroApp::get_version() const
-{
-    return version;
-}
-
-const std::string micro::MicroApp::get_url() const
-{
-    return url;
-}
-
-const std::string micro::MicroApp::get_author() const
-{
-    return author;
-}
-
-const std::string micro::MicroApp::get_email() const
-{
-    return email;
-}
-
-const std::string micro::MicroApp::get_description() const
-{
-    return description;
-}
-
-const std::string micro::MicroApp::get_long_description() const
-{
-    return long_description;
-}
-
-const std::string micro::MicroApp::get_help() const
-{
-    return help;
 }
