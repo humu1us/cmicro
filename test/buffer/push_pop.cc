edf5c273ac2a9befc0fae7d43898b3cc30c65396
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2019-05-03 23:15:53 -0400

    Add Buffer push and pop unit tests

---
+++ test/buffer/push_pop.cc
@@ -0,0 +1,71 @@
+#include <micro/core/buffer.h>
+#include <micro/core/message.h>
+
+#include <assert.h>
+
+int main()
+{
+    micro::Buffer *buffer = new micro::Buffer(2);
+    assert(buffer);
+    assert(buffer->is_empty());
+    assert(!buffer->is_full());
+    assert(buffer->size() == 0);
+
+    std::unique_ptr<micro::Message *> msg_1;
+    msg_1 = std::make_unique<micro::Message *>(new micro::Message(1, "", "", ""));
+    assert(msg_1);
+    assert((*msg_1)->get_iface_id() == 1);
+
+    buffer->push(std::move(msg_1));
+    assert(!msg_1);
+    assert(!buffer->is_empty());
+    assert(!buffer->is_full());
+    assert(buffer->size() == 1);
+
+    std::unique_ptr<micro::Message *> msg_2;
+    msg_2 = std::make_unique<micro::Message *>(new micro::Message(2, "", "", ""));
+    assert(msg_2);
+    assert((*msg_2)->get_iface_id() == 2);
+
+    buffer->push(std::move(msg_2));
+    assert(!msg_2);
+    assert(!buffer->is_empty());
+    assert(buffer->is_full());
+    assert(buffer->size() == 2);
+
+    std::unique_ptr<micro::Message *> msg_3;
+    msg_3 = std::make_unique<micro::Message *>(new micro::Message(3, "", "", ""));
+    assert(msg_3);
+    assert((*msg_3)->get_iface_id() == 3);
+
+    std::string error = std::string();
+    try
+    {
+        buffer->push(std::move(msg_3));
+    } catch (std::out_of_range &e)
+    {
+        error = e.what();
+    }
+    assert(error == "buffer is full");
+    assert(!msg_3);
+
+    std::unique_ptr<micro::Message *> msg_1_ = buffer->pop();
+    assert((*msg_1_)->get_iface_id() == 1);
+    assert(!buffer->is_empty());
+    assert(!buffer->is_full());
+    assert(buffer->size() == 1);
+
+    std::unique_ptr<micro::Message *> msg_2_ = buffer->pop();
+    assert((*msg_2_)->get_iface_id() == 2);
+    assert(buffer->is_empty());
+    assert(!buffer->is_full());
+    assert(buffer->size() == 0);
+
+    std::unique_ptr<micro::Message *> msg_3_ = buffer->pop();
+    assert(!msg_3_);
+    assert(buffer->is_empty());
+    assert(!buffer->is_full());
+    assert(buffer->size() == 0);
+
+    return 0;
+}
